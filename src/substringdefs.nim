d503ebd33230583ad5a1f7783e4fafd1d1355ebb
nirokay <nirokay-public@protonmail.com>
2025-08-03 13:13:45 +0200

    added bavaria posting to substring responses

--- src/substringdefs.nim
+++ src/substringdefs.nim
@@ -177,3 +177,56 @@ Many computer users run a modified version of the GNU system every day, without 
 
 There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!"""
 )
+
+add SubstringReaction(
+    name: "Bavaria posting",
+    trigger: @[
+        " Bavaria ", " Bavarian ",
+        " Bayern ", " bayerisch", "bayrisch"
+    ],
+    probability: 0.01,
+    emoji: "‼️",
+    response: """Bayern Bayern Bayern Bayern
+Ich sage euch eins: Bayern Bayern Bayern
+
+Bayern Bayern Bayern die Bayern für Bayern, ob in Bayern oder Bayern.
+Übrigens: Bayern!
+Bayern ist die Seele Bayerns - überall in Bayern, in Niederbayern, bei Wacker Südost Oberbayern, vor allem in Südbayern: Hauptsache Bayern.
+
+Bayern, Bayern ist einfach die Stimme Bayerns, für bayerische Interessen, in Bayern.
+Aber wisst ihr was? Bayern Bayern Bayern
+Bayern hat Bayern und... Bayern, was denn sonst?
+
+Der erste zentrale Punkt für uns ist: Bayern
+Das zweite große Mega-Thema: Bayern
+Und als letzten Punkt: Baden-Württemberg... ah nee... Bayern!
+Bayern allein, weil Bayern - als Bayern, für Bayern, mit den Bayern - Bayern erhält
+
+Wie schön Bayern ist, geh nach Bayern.
+In Bayern gibt's Bayern - nur in Bayern gibt's Bayern und sonst nirgendswo, für Bayern ist das wichtig!
+Stichwort Bayern: Bayern bedeutet bayerisches Bayern, die bayerische Kultur, die bayerische Lebensart, die Bayernbilanz und... das Bayerngefühl.
+
+Oh ja, man muss Bayern nicht mögen; Man muss Bayern lieben! Warum? Weil Bayern!
+
+Sind wir doch mal ehrlich: "Bayern" ist doch mit Abstand das schönste Wort der Welt oder?
+Gibt es ein wichtigeres Wort auf der Welt?
+
+Ich will nur eins sagen: Bayern Bayern Bayern Bayern
+Wenn Bayern, dann in Bayern.
+Bayern Bayern Bayern Bayern, nur Bayern!
+Wir brauchen Bayern in einem Land wie Bayern. Bayern fundamental, aber auch Bayern.
+
+Ein Bayer kam aus Bayern; Es war die Rettung Bayerns.
+Qas für ein Gänsehautmoment als dann die bayerische Hymne, die Bayernhymne, das Bayernlüb gesungen wurde.
+Übrigens von ganz, ganz tollen Bayern - mit Sombreros.
+
+huhu
+Bayern Bayern Bayern
+Wir wollen Bayern
+Bayern Bayern Bayern
+Gott schätze Bayern
+Bayern Bayern Bayern
+Bayern sei da schlecht... WAS?
+Bayern Bayern Bayern
+Unsinn und Schafscheiß"""
+)
