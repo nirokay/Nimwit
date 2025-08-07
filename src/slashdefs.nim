75e2804101ec28126e578559cf524e4b19a96009
nirokay <nirokay-public@protonmail.com>
2025-08-07 08:39:36 +0200

    date command pause

--- src/slashdefs.nim
+++ src/slashdefs.nim
@@ -430,21 +430,22 @@ add SlashCommand(
     call: slapSlash
 )
 
-add SlashCommand(
-    name: "date",
-    desc: "Asks a user out on a date.",
-    category: cat,
+when defined release:
+    add SlashCommand(
+        name: "date",
+        desc: "Asks a user out on a date.",
+        category: cat,
 
-    options: @[SlashOption(
-        kind: acotUser,
-        name: "user",
-        description: "User to ask out",
-        required: some true
-    )],
+        options: @[SlashOption(
+            kind: acotUser,
+            name: "user",
+            description: "User to ask out",
+            required: some true
+        )],
 
-    kind: atSlash,
-    call: slashDate
-)
+        kind: atSlash,
+        call: slashDate
+    )
 
 add SlashCommand(
     name: "cat",
